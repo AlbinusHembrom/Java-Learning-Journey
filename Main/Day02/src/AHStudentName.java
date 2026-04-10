17c9d900764f88ef2823c14cbcc5f63964b545d3
ALBINUS HEMBROM <albinushembrom47@gmail.com>
2026-04-10 15:02:38 +0530

    Update

--- Main/Day02/src/AHStudentName.java
+++ Main/Day02/src/AHStudentName.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 
-public class StudentName {
+public class AHStudentName {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
         // Taking name from the user
         System.out.println("Enter the Name: ");
-        String name = sc.nextLine();
+        String ahName = sc.nextLine();
 
         // Calculating length of name
-        int len = name.length();
+        int len = ahName.length();
 
         // Print the name and its length
-        System.out.println("Student name : " +name);
+        System.out.println("Student name : " +ahName);
         System.out.println("Length of the name : " +len);
 
         // Print the Student name in Upper and Lower case
-        System.out.println("Student name in Upper Case : " +name.toUpperCase());
-        System.out.println("Student name in Lower Case : " +name.toLowerCase());
+        System.out.println("Student name in Upper Case : " +ahName.toUpperCase());
+        System.out.println("Student name in Lower Case : " +ahName.toLowerCase());
         sc.close();
     }
 }
